55f142f907a056786ebef2b3a4c61da2f035cd61
Jayme Bird <jbird@influxdata.com>
2022-12-06 21:17:26 +0000

    add day 6

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,6 +1,5 @@
 use std::env;
 use std::fs;
-use std::str::Lines;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,9 +7,32 @@ fn main() {
 
     let contents = fs::read_to_string(config.file_path).expect("should have read this file");
 
-    let lines = contents.lines();
+    // let res1 = find_marker(contents, 4);
+    let res2 = find_marker(contents, 14);
 
+    // part 1
+    // println!("{}", res1);
 
+    println!("{}", res2);
+}
+
+fn find_marker(s: String, n:usize) -> usize {
+    let mut recent_chars = Vec::new();
+
+    for (i, c) in s.chars().enumerate() {
+        recent_chars.push(c);
+
+        if recent_chars.len() > n {
+            recent_chars.remove(0);
+        }
+
+        if recent_chars.len() == n {
+            if recent_chars.iter().all(|&c| recent_chars.iter().filter(|&&d| d == c).count() == 1) {
+                return i + 1;
+            }
+        }
+    }
+    return 0
 }
 
 
